e1245a996a68257a593cce5a9e3626d26e6830c3
Jwhitehead-drft <67030406+Jwhitehead-drft@users.noreply.github.com>
2021-03-30 10:16:15 -0400

    Added products to database to fill out page.

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,27 +1,27 @@
 const products = [
     {
     "_id": "1",
-    "description": "Lorem test test test",
+    "description": "Labradorite Tree of Life Pendant with Sterling Silver Wire wrap. No chain included.",
     "title":"Tree of Life Pendant",
     "brand": "Jake's Friend",
     "price": 95.00,
     "image": "../images/treependant1.PNG",
     "countInStock": 1,
-    "rating": 4.5,
+    "rating": 3.5,
     "numReviews": 3
 },{
     "_id": "2",
-    "description": "Lorem",
+    "description": "Labradorite Pendant with Sterling Silver Wire wrap. No chain included.",
     "title":"Deep Blue Pendant",
     "brand": "Jake's Friend",
     "price": 55.00,
     "image": "../images/deepbluependant.PNG",
     "countInStock": 0,
     "rating": 4.5,
-    "numReviews": 3
+    "numReviews": 1
 },{
     "_id": "3",
-    "description": "Lorem",
+    "description": "Dual Point Quartz Bronze Deer Skull Necklace Antiqued Bronze Chain 24 inch",
     "title":"Dual Point Quartz Bronze Deer Skull",
     "brand": "Jake's Friend",
     "price": 55.00,
@@ -31,7 +31,7 @@ const products = [
     "numReviews": 3
 },{
     "_id": "4",
-    "description": "Lorem",
+    "description": "Labradorite Pendant Antiqued Copper Tree of Life, No chain included.",
     "title":"Tree of Life Pendant",
     "brand": "Jake's friend",
     "price": 95.00,
@@ -40,7 +40,73 @@ const products = [
     "numReviews": 5
 },{
     "_id": "5",
-    "description": "Lorem",
+    "description": "Labradorite Antiqued and Bare Copper Tree of Life Pendant",
+    "title":"Tree of Life Pendant",
+    "brand": "Jake's friend",
+    "price": 95.00,
+    "image": "../images/treependant5.PNG",
+    "countInStock": 1,"rating": 4.0,
+    "numReviews": 2
+},{
+    "_id": "6",
+    "description": "Custom Request! Faux Leather Wrapped Diary with Inlays",
+    "title":"Custom Notebook",
+    "brand": "Jake's friend",
+    "price": 125.00,
+    "image": "../images/diary.PNG",
+    "countInStock": 1,"rating": 5.0,
+    "numReviews": 1
+},{
+    "_id": "7",
+    "description": "Labradorite Pendant Antiqued Copper Tree of Life, No chain included.",
+    "title":"Tree of Life Pendant",
+    "brand": "Jake's friend",
+    "price": 95.00,
+    "image": "../images/treependant4.PNG",
+    "countInStock": 1,"rating": 5.0,
+    "numReviews": 5
+},{
+    "_id": "8",
+    "description": "Labradorite Pendant with Sterling Silver Wire wrap. No chain included.",
+    "title":"Deep Blue Pendant",
+    "brand": "Jake's Friend",
+    "price": 55.00,
+    "image": "../images/deepbluependant.PNG",
+    "countInStock": 0,
+    "rating": 4.5,
+    "numReviews": 1
+},{
+    "_id": "9",
+    "description": "Dual Point Quartz Bronze Deer Skull Necklace Antiqued Bronze Chain 24 inch",
+    "title":"Dual Point Quartz Bronze Deer Skull",
+    "brand": "Jake's Friend",
+    "price": 55.00,
+    "image": "../images/skullpendant1.PNG",
+    "countInStock": 0,
+    "rating": 4.5,
+    "numReviews": 3
+},{
+    "_id": "10",
+    "description": "Labradorite Tree of Life Pendant with Sterling Silver Wire wrap. No chain included.",
+    "title":"Tree of Life Pendant",
+    "brand": "Jake's Friend",
+    "price": 95.00,
+    "image": "../images/treependant1.PNG",
+    "countInStock": 1,
+    "rating": 3.5,
+    "numReviews": 3
+},{
+    "_id": "11",
+    "description": "Custom Request! Faux Leather Wrapped Diary with Inlays",
+    "title":"Custom Notebook",
+    "brand": "Jake's friend",
+    "price": 125.00,
+    "image": "../images/diary.PNG",
+    "countInStock": 1,"rating": 5.0,
+    "numReviews": 1
+},{
+    "_id": "12",
+    "description": "Labradorite Antiqued and Bare Copper Tree of Life Pendant",
     "title":"Tree of Life Pendant",
     "brand": "Jake's friend",
     "price": 95.00,
